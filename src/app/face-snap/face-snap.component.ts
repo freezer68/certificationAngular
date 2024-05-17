858c949d5f74782ae9c81d7cd84cc62ead7f3e91
freezer68 <freezer68@gmail.com>
2024-05-17 13:45:05 +0200

    Ajout de données dans un composant. Ajout de propriétées aux composant et déclaration de ses composants avec ngOnInit

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+title!:string;
+description!:string;
+createdDate!:Date;
+snaps!:number;
 
+ngOnInit() {
+
+  this.title = 'Archibald';
+  this.description = 'Une peluche';
+  this.createdDate = new Date();
+  this.snaps = 6;
+  
+}
 }
